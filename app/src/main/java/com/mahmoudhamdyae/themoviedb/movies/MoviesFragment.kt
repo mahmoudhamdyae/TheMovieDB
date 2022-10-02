7a40b3ed3a98e10b418759a0b9750d7109fa73e4
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-10-02 15:36:45 +0200

    Add-Movies-Fragment

---
+++ app/src/main/java/com/mahmoudhamdyae/themoviedb/movies/MoviesFragment.kt
@@ -0,0 +1,7 @@
+package com.mahmoudhamdyae.themoviedb.movies
+
+import androidx.fragment.app.Fragment
+import com.mahmoudhamdyae.themoviedb.R
+
+class MoviesFragment : Fragment (R.layout.fragment_movies) {
+}
