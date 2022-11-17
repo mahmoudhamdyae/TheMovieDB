d928e17471b969e2b5ccf4bf4aa55dee15b60b8f
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2022-11-17 19:32:33 +0200

    Some_UI_Changes

--- app/src/main/java/com/mahmoudhamdyae/themoviedb/all/AllFragment.kt
+++ app/src/main/java/com/mahmoudhamdyae/themoviedb/all/AllFragment.kt
@@ -1,12 +1,17 @@
 package com.mahmoudhamdyae.themoviedb.all
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mahmoudhamdyae.themoviedb.R
 import com.mahmoudhamdyae.themoviedb.databinding.FragmentAllBinding
 
 class AllFragment: Fragment() {
@@ -29,6 +34,7 @@ class AllFragment: Fragment() {
             findNavController().navigateUp()
         }
 
+        binding.photosGrid.layoutManager = GridLayoutManager(context, 1)
         binding.photosGrid.adapter = AllAdapter(AllAdapter.OnClickListener {
             findNavController().navigate(AllFragmentDirections.actionAllFragmentToDetailFragment(it))
         })
