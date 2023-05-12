77341060e1bbd18610db9185df48228fb821ce4d
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-05-12 09:11:13 +0300

    Dependency Injection Refactor

--- app/src/main/java/com/mahmoudhamdyae/themoviedb1/di/RepositoryModule.kt
+++ app/src/main/java/com/mahmoudhamdyae/themoviedb1/di/RepositoryModule.kt
@@ -1,20 +1,16 @@
 package com.mahmoudhamdyae.themoviedb1.di
 
-import com.google.firebase.auth.FirebaseAuth
-import com.mahmoudhamdyae.themoviedb1.data.network.ApiService
 import com.mahmoudhamdyae.themoviedb1.data.repository.Repository
 import com.mahmoudhamdyae.themoviedb1.data.repository.RepositoryImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
-    fun provideRepository(mAuth: FirebaseAuth, apiService: ApiService): Repository {
-        return RepositoryImpl(mAuth, apiService)
-    }
+    @Binds
+    abstract fun provideRepository(impl: RepositoryImpl): Repository
 }
